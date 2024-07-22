d46fbc463ccdf698ef3173debaf68a1290bd062e
shooore <shooore@gmail.com>
2024-07-22 10:30:46 +0300

    + kv

---
+++ site/sunmar.ru/js/hero-section.js
@@ -0,0 +1,9 @@
+import { hostReactAppReady } from "../../common/js/usefuls";
+
+(async function () {
+    await hostReactAppReady();
+    for (const hero of document.querySelectorAll('section.held')) {
+        const row_container = hero.closest('.row-container');
+        if (row_container) row_container.style.overflow = 'visible';
+    }
+})();
